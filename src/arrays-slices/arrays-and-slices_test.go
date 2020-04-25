ca6f7f0c56e774c624ba9dbcecfa7cc707201e5c
medmes <mohammed.mesaoudi@gmail.com>
2020-04-25 15:31:28 +0000

    Arrays and Slices - Refactoring, Removing redundant test code, because basically they do the same goal ! 'go test -cover' -> will give 100.0% with keeping the second test code or not.

--- src/arrays-slices/arrays-and-slices_test.go
+++ src/arrays-slices/arrays-and-slices_test.go
@@ -15,17 +15,4 @@ func TestSum(t *testing.T) {
 			t.Errorf("want : [ %d ] but got : [ %d ] , given : %v !", want, got, numbers)
 		}
 	})
-
-	t.Run("Sum of collection of any numbers", func(t *testing.T) {
-		// Given
-		numbers := []int{1, 2, 3}
-		want := 6
-		//When
-		got := Sum(numbers)
-
-		// Asserting , Then
-		if got != want {
-			t.Errorf("want : [ %d ] but got : [ %d ] , given : %v !", want, got, numbers)
-		}
-	})
 }
