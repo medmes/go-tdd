aa35b8aa833102e135aa126c62eac14b6d1f22cf
medmes <mohammed.mesaoudi@gmail.com>
2020-04-25 15:57:47 +0000

    Arrays and Slices - Adding  Sum All Tails function which allow us to sum each slice with its last numbers.

--- src/arrays-slices/arrays-and-slices.go
+++ src/arrays-slices/arrays-and-slices.go
@@ -18,3 +18,12 @@ func SumAll(numbers ...[]int) (sums []int) {
 	}
 	return
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
+}
--- src/arrays-slices/arrays-and-slices_test.go
+++ src/arrays-slices/arrays-and-slices_test.go
@@ -6,18 +6,16 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("Sum of collection of 5 numbers", func(t *testing.T) {
-		// Given
-		numbers := []int{1, 2, 3, 4, 5}
-		want := 15
-		//When
-		got := Sum(numbers)
+	// Given
+	numbers := []int{1, 2, 3, 4, 5}
+	want := 15
+	//When
+	got := Sum(numbers)
 
-		// Asserting , Then
-		if got != want {
-			t.Errorf("want : [ %d ] but got : [ %d ] , given : %v !", want, got, numbers)
-		}
-	})
+	// Asserting , Then
+	if got != want {
+		t.Errorf("want : [ %d ] but got : [ %d ] , given : %v !", want, got, numbers)
+	}
 }
 
 func TestSumAll(t *testing.T) {
@@ -29,5 +27,16 @@ func TestSumAll(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("want :  %d but got :  %d !", want, got)
 
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	// Given, When
+	want := []int{2, 9}
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+
+	// Asserting, Then
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
