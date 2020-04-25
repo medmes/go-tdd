3baf95e8a4d6fb7e3d59dc8c2c98f61fe622c1da
medmes <mohammed.mesaoudi@gmail.com>
2020-04-25 16:08:55 +0000

    Arrays and Slices - Refactoring TestSumAllTails, Sum All Tails function with keeping in mind empty slice .

--- src/arrays-slices/arrays-and-slices_test.go
+++ src/arrays-slices/arrays-and-slices_test.go
@@ -30,15 +30,21 @@ func TestSumAll(t *testing.T) {
 	}
 }
 
+//Our tests have some repeated code around assertion again, let's extract that into a function
+// So we need to optimize our test code:
 func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
 	t.Run("make the sums of some slices", func(t *testing.T) {
 		want := []int{2, 9}
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 
 		// Asserting
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
@@ -46,8 +52,6 @@ func TestSumAllTails(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 
 		// Asserting
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
