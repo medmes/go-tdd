490c3e3b4b2af3148084de62d98db4d961929c42
medmes <mohammed.mesaoudi@gmail.com>
2020-04-29 13:51:37 +0000

    Methods and Structs - Adding receiver name/receiver type to attach a function to a struct.

--- src/struct-interface/struct-interface.go
+++ src/struct-interface/struct-interface.go
@@ -1,14 +1,26 @@
 package struct_interface
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+// Rectangle methods
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle methods
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- src/struct-interface/struct-interface_test.go
+++ src/struct-interface/struct-interface_test.go
@@ -15,11 +15,29 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	assertThat := func(t *testing.T, got, want float64) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
 	}
+	t.Run("Calculate rectangle perimeter", func(t *testing.T) {
+		// Given, When
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		//Asserting
+		assertThat(t, got, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		// Given, When
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		//Asserting
+		assertThat(t, got, want)
+	})
 }
