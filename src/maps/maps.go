ae50090e02dc7b1580aac8df667b71334be0f423
medmes <mohammed.mesaoudi@gmail.com>
2020-05-06 21:21:39 +0000

    Map - clarification of how to initialize a map

--- src/maps/maps.go
+++ src/maps/maps.go
@@ -4,6 +4,11 @@ import "errors"
 
 var NotFoundError error = errors.New("not Found word")
 
+// var m map[string]string // never initialize an empty map, it might generate a nil panic
+// Initialize
+// var dictionary = map[string]string{}
+// OR
+// var dictionary = make(map[string]string)
 type Dictionary map[string]string
 
 func (d Dictionary) Search(w string /* word */) (string, error) {
