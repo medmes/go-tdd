ad673169b86fa0f2c914cf42fc1ef7ce98e3dba1
medmes <mohammed.mesaoudi@gmail.com>
2020-05-26 19:02:08 +0000

    DI - Poet example, strong dependency injection problem.

--- di/di.go
+++ di/di.go
@@ -7,6 +7,27 @@ import (
 	"net/http"
 )
 
+type Poem struct {
+	content []byte
+	// let's consider Poems have to be stored somewhere
+	//acmeStorageServices is a service that handle storage operation
+	storage acmeStorageServices.PoemNotebook
+}
+
+func NewPoem() *Poem {
+	return &Poem{
+		storage: acmeStorageServices.NewPoemNotebook(),
+	}
+}
+
+func (p *Poem) Load(title string) {
+	p.content = p.storage.Load(title)
+}
+
+func (p *Poem) Save(title string) {
+	p.storage.Save(title)
+}
+
 func Greet(writer io.Writer /* interface */, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
